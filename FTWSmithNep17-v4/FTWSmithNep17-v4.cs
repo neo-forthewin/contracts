6dda615a6920e84332faad6f32be53829b263916
Forthewin <hello@forthewin.network>
2024-10-30 00:41:31 -0700

    Add FTWSmithNep17 v3 and v4 contracts and csproj files

---
+++ FTWSmithNep17-v4/FTWSmithNep17-v4.cs
@@ -0,0 +1,70 @@
+﻿using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Services;
+using System.Numerics;
+
+namespace FTWContracts
+{
+    [SupportedStandards("NEP-17")]
+    [ContractPermission("*", "*")]
+    public partial class FTWSmithNep17 : Nep17Token
+    {
+        protected const string Prefix_Symbol = "symbol";
+        protected const string Prefix_Decimals = "decimals";
+        protected const string Prefix_Version = "version";
+        protected const string VERSION = "v4";
+
+        [Safe]
+        public override string Symbol()
+        {
+            return (string)Storage.Get(Storage.CurrentContext, Prefix_Symbol.ToByteArray());
+        }
+
+        [Safe]
+        public override byte Decimals()
+        {
+            var decimals = (BigInteger)
+                Storage.Get(Storage.CurrentContext, Prefix_Decimals.ToByteArray());
+            return decimals.ToByte();
+        }
+
+        [Safe]
+        public static string Version() => VERSION;
+
+        public static void Init(
+            UInt160 contractOwner,
+            string symbol,
+            int decimals,
+            BigInteger totalSupply
+        )
+        {
+            if (Storage.Get(Storage.CurrentContext, Prefix_Symbol.ToByteArray()) != null)
+            {
+                ExecutionEngine.Assert(false, "Contract already initiated.");
+            }
+
+            Storage.Put(Storage.CurrentContext, Prefix_Symbol.ToByteArray(), symbol);
+            Storage.Put(Storage.CurrentContext, Prefix_Decimals.ToByteArray(), decimals);
+            Storage.Put(
+                Storage.CurrentContext,
+                Prefix_Version.ToByteArray(),
+                VERSION.ToByteArray()
+            );
+            BigInteger _decimals = BigInteger.Pow(10, decimals);
+            Mint(contractOwner, totalSupply * _decimals);
+        }
+
+        public static void Burn(UInt160 from, BigInteger amount)
+        {
+            ExecutionEngine.Assert(Runtime.CheckWitness(from), "CheckWitness failed.");
+            Burn(from, amount);
+        }
+
+        public static void _deploy(object data, bool update)
+        {
+            if (update)
+                return;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Forthewin Public Smart Contracts
+
+Our primary smart contracts are kept in a private repository. Some are public for verifications.
